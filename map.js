35e74957293b92a1b9994132da6001a5bbffc699
Aoga <lathuantrien@gmail.com>
2023-03-11 03:37:51 +1100

    remove Center marker

--- map.js
+++ map.js
@@ -47,7 +47,6 @@ interactive_map.addTileLayer('Ingame map', {
 // See `marker_logic/collectibles.js` for a really basic layer
 //addCollectibles(interactive_map);
 addRavenna(interactive_map);
-addCenter(interactive_map);
 
 // Step 3.5 (optional):
 // Add more marker layer
